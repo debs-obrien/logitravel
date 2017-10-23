3d4b0f3f765a3410f925da29bd3463840a4e0a96
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-10-23 17:28:39 +0200

    pug and routes finished

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const sassMiddleware = require('node-sass-middleware');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const paquetes = require('./routes/paquetes');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,11 +30,22 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/paquetes', paquetes);
+
+//just for route testing these should have their own pug and routes
+app.use('/cruceros', index);
+app.use('/costas', index);
+app.use('/parques', index);
+app.use('/chollos', index);
+app.use('/vuelos', index);
+app.use('/hoteles', index);
+app.use('/trenes', index);
+app.use('/ferries', index);
+app.use('/destinos', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
