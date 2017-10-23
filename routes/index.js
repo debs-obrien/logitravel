c3c622bc6923702857649721610aa5ebbeb2565d
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-10-23 16:41:56 +0200

    card pug finished

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const { data } = require('../public/data/data.json');
+const { cards } = data;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  res.render('index', { title: 'Express' });
+    console.log(cards);
+  res.render('index', data);
 });
 
 module.exports = router;
